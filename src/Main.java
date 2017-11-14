cc04e28d44462dbc643306ed401436c64150cb66
FrogTravel <e.levchenko@innopolis.ru>
2017-11-14 14:29:07 +0300

    Repetition code added

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,22 @@ import java.net.Socket;
 public class Main {
 
     public static void main(String args[]) throws IOException, InterruptedException {
-        Messenger messenger = new Messenger();
 
         byte[] message = {1, 2, 3, 42, 4, 5};
+
+        RepetitionCoding conding = new RepetitionCoding();
+
+        byte[] result = conding.encode(message, 3);
+
+        byte[] decoded = conding.decode(result, 3);
+
+        for (int i = 0; i < decoded.length; i++) {
+            System.out.println(decoded[i]);
+
+        }
+
+        Messenger messenger = new Messenger();
+
         messenger.sendMessageByte(message);
 
         Thread.sleep(1000);
